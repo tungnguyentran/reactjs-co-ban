debd77fe40afa0ac3b6f7e1806612831d29f28b3
tungnt1 <tungnt1@gearinc.com>
2022-06-18 21:08:59 +0700

    init project

--- src/index.js
+++ src/index.js
@@ -4,12 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const user = {
+  firstName: "Tung",
+  lastName: "Nguyen"
+}
+
+function formatName(user) {
+  return `${user.firstName} ${user.lastName}`;
+}
+
+const myStyle = {
+  color: "white",
+  backgroundColor: "DodgerBlue",
+  padding: "10px",
+  fontFamily: "Arial"
+};
+
+const element = (
+  <h1>Hello, {formatName(user)}</h1>
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<div style={{
+  color: "white",
+  backgroundColor: "DodgerBlue",
+  padding: "10px",
+  fontFamily: "Arial"
+}} children={<div className='white'></div>} className='black'></div>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
